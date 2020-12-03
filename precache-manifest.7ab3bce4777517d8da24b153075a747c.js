3c20e3feeabbfdcd8f3e053a815a4c5c9051d892
tribetron <programmertatenda@gmail.com>
2020-12-03 07:36:23 -0800

    Updates

---
+++ precache-manifest.7ab3bce4777517d8da24b153075a747c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b3f9e544fae091305b",
+    "url": "/oldmutual/static/css/main.b78e9f02.chunk.css"
+  },
+  {
+    "revision": "76b3f9e544fae091305b",
+    "url": "/oldmutual/static/js/main.76b3f9e5.chunk.js"
+  },
+  {
+    "revision": "89e8a3583459bcb2a5a3",
+    "url": "/oldmutual/static/js/runtime~main.89e8a358.js"
+  },
+  {
+    "revision": "02a252e04d5474d39481",
+    "url": "/oldmutual/static/css/2.61cd052a.chunk.css"
+  },
+  {
+    "revision": "02a252e04d5474d39481",
+    "url": "/oldmutual/static/js/2.02a252e0.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/oldmutual/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "1f1ef0d1be154dead31d859faec5e0a5",
+    "url": "/oldmutual/index.html"
+  }
+];
